953ff0fa1c5239d6d68b2d2b3ecb638f5e722656
hanchaoqun <hanssccv@gmail.com>
2023-05-01 13:40:57 +0800

    change edge location

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -20,4 +20,34 @@ export async function POST(req: NextRequest) {
   });
 }
 
-export const runtime = "edge";
+
+// export const runtime = "edge";
+
+export const config = {
+  runtime: "edge",
+  /**
+   * https://vercel.com/docs/concepts/edge-network/regions#region-list
+   * disable hongkong
+   * only for vercel
+   */
+  regions: [
+    "arn1",
+    "bom1",
+    "bru1",
+    "cdg1",
+    "cle1",
+    "cpt1a",
+    "dub1",
+    "fra1",
+    "gru1",
+    "hnd1",
+    "iad1",
+    "icn1",
+    "kix1",
+    "lhr1",
+    "pdx1",
+    "sfo1",
+    "sin1",
+    "syd1"
+  ]
+}
