d444c687f7d4cae9cb3a2e1f881b93b65b657117
hanchaoqun <hanssccv@gmail.com>
2023-05-01 17:14:05 +0800

    fix input width 2

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -56,7 +56,7 @@ export function ModelConfigList(props: {
           type="number"
           min={100}
           max={32000}
-          width="80"
+          width="120"
           value={props.modelConfig.max_tokens}
           onChange={(e) =>
             props.updateConfig(
@@ -115,7 +115,7 @@ export function ModelConfigList(props: {
           type="number"
           min={500}
           max={4000}
-          width="80"
+          width="120"
           value={props.modelConfig.compressMessageLengthThreshold}
           onChange={(e) =>
             props.updateConfig(
