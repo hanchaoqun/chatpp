a9f3084d4e8b959818e0cfb8bb33948f11da64c9
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2023-05-02 17:56:44 +0300

    Update chat.ts

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -500,8 +500,8 @@ export const useChatStore = create<ChatStore>()(
             newSession.topic = oldSession.topic;
             newSession.messages = [...oldSession.messages];
             newSession.mask.modelConfig.sendMemory = true;
-            newSession.mask.modelConfig.historyMessageCount = 4;
-            newSession.mask.modelConfig.compressMessageLengthThreshold = 1000;
+            newSession.mask.modelConfig.historyMessageCount = 5;
+            newSession.mask.modelConfig.compressMessageLengthThreshold = 2000;
             newState.sessions.push(newSession);
           }
         }
