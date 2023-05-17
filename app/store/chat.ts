94c9dae2444f1267b27cd1494bbf9cc1d9411a73
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2023-05-17 23:57:13 +0300

    hard limit to less than 4000

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -331,6 +331,7 @@ export const useChatStore = create<ChatStore>()(
         let sumTokens = 0;
         // HARD LIMIT !!!!
         let hardMaxTokens = (maxTokens > 0) ? Math.floor(maxTokens * 0.25) : 0;
+        hardMaxTokens = (hardMaxTokens > 4000) ? 4000: hardMaxTokens;
         // HARD LIMIT !!!!
         for (let i = messages.length - 1; i >= 0; i--) {
           const tks = get().getMessagesTokens(messages[i]);
