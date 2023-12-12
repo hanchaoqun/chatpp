16492c9b467ca0abc0c8dbc53352c53108ef4a0a
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2023-12-12 18:21:48 +0300

    Update requests.ts

--- app/requests.ts
+++ app/requests.ts
@@ -62,7 +62,7 @@ const makeRequestParam = (
         model: modelConfig.model,
         temperature: modelConfig.temperature,
         presence_penalty: modelConfig.presence_penalty,
-        max_tokens: 4096,
+        max_tokens: 1024,
       };
   }
   return {
