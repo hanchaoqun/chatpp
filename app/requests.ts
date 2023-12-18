d48ba1d7152777b30f81a9ed775456cce294b221
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2023-12-18 20:27:12 +0300

    refactor

--- app/requests.ts
+++ app/requests.ts
@@ -1,13 +1,17 @@
 import {
-  Message,
-  ImageUrl,
-  ImageContent,
   ModelConfig,
   ModelType,
   useAccessStore,
   useAppConfig,
   useChatStore,
 } from "./store";
+import {
+  Message,
+  ImageUrl,
+  ImageContent,
+  ChatRequest,
+  ChatResponse,
+} from "./api/type/typing";
 import { showToast } from "./components/ui-lib";
 
 const TIME_OUT_MS = 60000;
@@ -63,7 +67,7 @@ const makeRequestParam = (
         temperature: modelConfig.temperature,
         presence_penalty: modelConfig.presence_penalty,
         max_tokens: 1024,
-      };
+      } as ChatRequest;
   }
   return {
     messages: [...sendMessages, {
@@ -74,7 +78,7 @@ const makeRequestParam = (
     model: modelConfig.model,
     temperature: modelConfig.temperature,
     presence_penalty: modelConfig.presence_penalty,
-  };
+  } as ChatRequest;
 };
 
 function getHeaders() {
@@ -93,20 +97,6 @@ function getHeaders() {
   return headers;
 }
 
-export function requestOpenaiClient(path: string) {
-  return (body: any, method = "POST") =>
-    fetch("/api/openai", {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-        path,
-        "model": (!body?.model)? "" : body.model,
-        ...getHeaders(),
-      },
-      body: body && JSON.stringify(body),
-    });
-}
-
 export async function requestChat(
   historyMessages: Message[],
   userMessage: Message,
@@ -119,64 +109,21 @@ export async function requestChat(
     model: options?.model,
   });
 
-  const res = await requestOpenaiClient("v1/chat/completions")(req);
-
   try {
+    const res = await fetch("/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "model": (!req?.model)? "" : req.model,
+          ...getHeaders(),
+        },
+        body: JSON.stringify(req),
+      });
     const response = await res.json();
     return response;
   } catch (error) {
     console.error("[Request Chat] ", error, res.body);
   }
-}
-
-export async function requestUsage() {
-  const formatDate = (d: Date) =>
-    `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, "0")}-${d
-      .getDate()
-      .toString()
-      .padStart(2, "0")}`;
-  const ONE_DAY = 2 * 24 * 60 * 60 * 1000;
-  const now = new Date(Date.now() + ONE_DAY);
-  const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-  const startDate = formatDate(startOfMonth);
-  const endDate = formatDate(now);
-
-  const [used, subs] = await Promise.all([
-    requestOpenaiClient(
-      `dashboard/billing/usage?start_date=${startDate}&end_date=${endDate}`,
-    )(null, "GET"),
-    requestOpenaiClient("dashboard/billing/subscription")(null, "GET"),
-  ]);
-
-  const response = (await used.json()) as {
-    total_usage?: number;
-    error?: {
-      type: string;
-      message: string;
-    };
-  };
-
-  const total = (await subs.json()) as {
-    hard_limit_usd?: number;
-  };
-
-  if (response.error && response.error.type) {
-    showToast(response.error.message);
-    return;
-  }
-
-  if (response.total_usage) {
-    response.total_usage = Math.round(response.total_usage) / 100;
-  }
-
-  if (total.hard_limit_usd) {
-    total.hard_limit_usd = Math.round(total.hard_limit_usd * 100) / 100;
-  }
-
-  return {
-    used: response.total_usage,
-    subscription: total.hard_limit_usd,
-  };
 }
 
 export async function requestChatStream(
@@ -209,7 +156,6 @@ export async function requestChatStream(
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        path: "v1/chat/completions",
         "model": (!req?.model)? "" : req.model,
         ...getHeaders(),
       },
@@ -280,9 +226,8 @@ export async function requestWithPrompt(
       tokens: 0,
   };
 
-  const res = await requestChat(historyMessages, userMessage, options);
-
-  return res?.choices?.at(0)?.message?.content ?? "";
+  const res = await requestChat(historyMessages, userMessage, options) as ChatResponse;
+  return res?.content??"";
 }
 
 // To store message streaming controller
