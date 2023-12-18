f86060062fd57b67ba69f221a90a2e2adbee0260
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2023-12-18 20:28:55 +0300

    refactor

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,15 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-import { requestOpenai } from "../common";
+import { request } from "../llm/sdk";
 
 async function makeRequest(req: NextRequest) {
   try {
-    const api = await requestOpenai(req);
+    const model = req.headers.get("model");
+    const api = await request(model, req, false);
     const res = new NextResponse(api.body);
     res.headers.set("Content-Type", "application/json");
     res.headers.set("Cache-Control", "no-cache");
     return res;
   } catch (e) {
-    console.error("[OpenAI] ", req.body, e);
+    console.error("[ERROR] ", req.body, e);
     return NextResponse.json(
       {
         error: true,
